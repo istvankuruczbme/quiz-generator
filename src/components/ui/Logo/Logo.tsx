3aca47e3dbd41c590019732ae28910823fbaa7a2
István Kurucz <kurucz.isti63@gmail.com>
2025-11-28 20:46:44 +0100

    Logo and favicon created.

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,7 +1,6 @@
 import { FC, HTMLAttributes } from "react";
 import addPropClassName from "../../../utils/addPropClassName";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLightbulb } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../../assets/logo.png";
 import "./Logo.css";
 
 type LogoProps = HTMLAttributes<HTMLDivElement>;
@@ -9,7 +8,7 @@ type LogoProps = HTMLAttributes<HTMLDivElement>;
 const Logo: FC<LogoProps> = ({ className }) => {
 	return (
 		<div className={`logo${addPropClassName(className)}`}>
-			<FontAwesomeIcon icon={faLightbulb} className="logo__icon" />
+			<img src={logo} alt="Logo" className="logo__img" />
 			<span className="logo__text">Quiz generator</span>
 		</div>
 	);
